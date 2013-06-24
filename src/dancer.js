b4929d46fa8adf6a02cb793c70fb848f7daca44f
Hack Reactor Students <github@hackreactor.com>
2013-06-24 12:15:47 -0700

    Initial commit; refactored dancer.js using pseudoclassical style.

--- src/dancer.js
+++ src/dancer.js
@@ -1,32 +1,32 @@
 // Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps){
-
-  var dancer = {};
-
+var Dancer = function(top, left, timeBetweenSteps){
+  this._dancer = {};
+  this._top = top;
+  this._left = left;
+  this._timeBetweenSteps = timeBetweenSteps;
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
+  this.$node = $('<span class="dancer"></span>');
+  this.step();
+  this.setPosition(this._top, this._left);
+};
 
-  dancer.step = function(){
+Dancer.prototype.step = function(){
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
-  };
-  dancer.step();
+  setTimeout(this.step, this._timeBetweenSteps);
+};
 
-  dancer.setPosition = function(top, left){
+
+Dancer.prototype.setPosition = function(top, left){
     /* Use css top and left properties to position our <span> tag
      * where it belongs on the page. See http://api.jquery.com/css/
      */
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    dancer.$node.css(styleSettings);
+  var styleSettings = {
+    top: top,
+    left: left
   };
+  this.$node.css(styleSettings);
+};
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
-
-  return dancer;
-};
