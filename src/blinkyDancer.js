8b01d07b90999cc6dcb1e7aa4732c9a4b035b109
Hack Reactor Students <github@hackreactor.com>
2013-06-24 13:27:29 -0700

    moving to pseudoclassical style

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,17 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps){
-  var blinkyDancer = new Dancer(top, left, timeBetweenSteps);
+var BlinkyDancer = function(top, left, timeBetweenSteps){
+  this._blinkyDancer = new Dancer(top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-  var oldStep = blinkyDancer.step;
+  this._oldStep = this.step;
+};
 
-  blinkyDancer.step = function(){
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
+BlinkyDancer.prototype.step = function(){
+  // call the old version of step at the beginning of any call to this new version of step
+  this._oldStep();
 
-    /* toggle() is a jQuery method to show/hide the <span> tag.
-     * See http://api.jquery.com/category/effects/ for this and
-     * other effects you can use on a jQuery-wrapped html tag. */
-    blinkyDancer.$node.toggle();
-  };
-
-  return blinkyDancer;
+  /* toggle() is a jQuery method to show/hide the <span> tag.
+   * See http://api.jquery.com/category/effects/ for this and
+   * other effects you can use on a jQuery-wrapped html tag. */
+  this.$node.toggle();
 };
