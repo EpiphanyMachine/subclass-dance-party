ff4c3557ffe97cd3640c6420980855fb1f92e144
Hack Reactor Students <github@hackreactor.com>
2013-06-24 17:33:43 -0700

    Added a second dancer with CSS class specification.

--- src/dancers.js
+++ src/dancers.js
@@ -20,4 +20,25 @@ BlinkyDancer.prototype.step = function(context){
   context.$node.toggle();
 };
 
+var ColorChangingDancer = function(top, left, timeBetweenSteps){
+  $('.dancer').addClass('circleDancer');
+  Dancer.call(this, top, left, timeBetweenSteps);
 
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+};
+
+ColorChangingDancer.prototype = new Dancer();
+ColorChangingDancer.prototype.constructor = ColorChangingDancer;
+ColorChangingDancer.prototype.oldStep = Dancer.prototype.step;
+ColorChangingDancer.prototype.step = function(context){
+  // call the old version of step at the beginning of any call to this new version of step
+  context = context || this;
+  // console.log(context);
+  context.oldStep(context);
+
+  /* toggle() is a jQuery method to show/hide the <span> tag.
+   * See http://api.jquery.com/category/effects/ for this and
+   * other effects you can use on a jQuery-wrapped html tag. */
+  context.$node.toggle();
+};
